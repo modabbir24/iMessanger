8e91f5686edf8554eb8f35b4f3c0add983d29eca
Modabbir Tarique <43771389+modabbir24@users.noreply.github.com>
2023-09-02 22:06:29 +0530

    Add files via upload
    Initial Setup

---
+++ iMessanger/iMessanger/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iMessanger
+//
+//  Created by Modabbir Tarique on 02/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
